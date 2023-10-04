7f8fdeb5721027170a13dc552cd0a4d97324d4b0
KHarrisDev <kharrisjr.dev@gmail.com>
2023-10-04 17:13:58 -0400

    update tsconfig.file
    Reviewing including and excluding files

---
+++ ts-compiler-configuration/analytics.ts
@@ -0,0 +1,13 @@
+// **********Compiling project/multiple files
+// import this js to index.html file
+// use following console.log to compile
+console.log("Sending...");
+// run "tsc --init" to run any TypeScript file that has been updated
+// creates a tsconfig.json file the declares the folder it's in is managed by Type
+
+
+// now we can run "tsc" in the terminal and all ts files will be compiled.
+// run "tsc -w" to enter watch mode
+
+// ! See tsconfig.json
+
